eef83098103f4a52b327c23c09d4cb2a6e3179e0
Ariya Seng <natsumi@gmail.com>
2016-05-09 20:22:37 -0700

    add spacemacs conf

--- install.sh
+++ install.sh
@@ -25,6 +25,7 @@ ln -s $PWD/vim/plugins ~/.vim/plugins
 ln -s $PWD/vim/vimrc.symlink ~/.vimrc
 ln -s $PWD/vim/ignore.vim.symlink ~/.vim/ignore.vim
 ln -s $PWD/vim/vimrc.symlink ~/.config/nvim/init.vim
+ln -s $PWD/spacemacs/spacemacs.symlink ~/.spacemacs
 
 ln -s $PWD/rails/.gemrc ~/
 ln -s $PWD/rails/.pryrc ~/
