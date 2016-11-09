e218ff92412f715031838d224537efe1a0ff2cf1
Ariya Seng <natsumi@gmail.com>
2016-11-09 10:27:57 -0800

    install airbnb global

--- install.sh
+++ install.sh
@@ -78,7 +78,7 @@ nvm alias default node
 npm install -g tern js-beautify
 npm install -g eslint babel-eslint
 export PKG=eslint-config-airbnb;
-npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --save-dev "$PKG@latest"
+npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install -g "$PKG@latest"
 
 # Install offical react project generator
 npm install -g create-react-app
