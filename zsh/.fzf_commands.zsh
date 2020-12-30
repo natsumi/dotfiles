469d35cdcc2ef833f120435e59c3b3225744c437
natsumi <natsumi@gmail.com>
2020-12-29 17:24:39 -0800

    fzf functions

--- zsh/.fzf_commands.zsh
+++ zsh/.fzf_commands.zsh
@@ -32,3 +32,17 @@ fco() {
   branch=$(echo "$branches" | fzf +m) &&
   git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
 }
+
+# Use fd (https://github.com/sharkdp/fd) instead of the default find
+# command for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
+
