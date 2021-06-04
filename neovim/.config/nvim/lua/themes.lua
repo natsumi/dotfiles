156c7ba63e79c38f5bd51b377260e5ba699690fe
natsumi <natsumi@gmail.com>
2021-06-03 23:19:15 -0700

    use nord

--- neovim/.config/nvim/lua/themes.lua
+++ neovim/.config/nvim/lua/themes.lua
@@ -18,23 +18,23 @@
 -- Moonlight
 -------------
 -- https://github.com/shaunsingh/moonlight.nvim
-vim.g.moonlight_italic_comments = true
-vim.g.moonlight_italic_keywords = true
-vim.g.moonlight_italic_functions = true
-vim.g.moonlight_italic_variables = false
-vim.g.moonlight_contrast = true
-vim.g.moonlight_borders = false
-vim.g.moonlight_disable_background = false
+-- vim.g.moonlight_italic_comments = true
+-- vim.g.moonlight_italic_keywords = true
+-- vim.g.moonlight_italic_functions = true
+-- vim.g.moonlight_italic_variables = false
+-- vim.g.moonlight_contrast = true
+-- vim.g.moonlight_borders = false
+-- vim.g.moonlight_disable_background = false
 
--- -- load the colorscheme
-require('moonlight').set()
+-- -- -- load the colorscheme
+-- require('moonlight').set()
 
 -------------
 -- Nord
 -------------
--- vim.g.nord_contrast = true
--- vim.g.nord_borders = false
--- vim.g.nord_disable_background = false
+vim.g.nord_contrast = true
+vim.g.nord_borders = false
+vim.g.nord_disable_background = false
 
--- -- Load the colorscheme
--- require('nord').set()
+-- Load the colorscheme
+require('nord').set()
