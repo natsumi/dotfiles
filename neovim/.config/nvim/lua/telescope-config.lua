cab3334d43b4f7b8e4f986fd7ef6d894b4a61209
natsumi <natsumi@gmail.com>
2021-07-03 21:12:46 -0700

    use updated telescope config values

--- neovim/.config/nvim/lua/telescope-config.lua
+++ neovim/.config/nvim/lua/telescope-config.lua
@@ -10,7 +10,6 @@ require("telescope").setup {
       "--column",
       "--smart-case"
     },
-    prompt_position = "bottom",
     prompt_prefix = " ",
     selection_caret = " ",
     entry_prefix = "  ",
@@ -18,23 +17,23 @@ require("telescope").setup {
     selection_strategy = "reset",
     sorting_strategy = "descending",
     layout_strategy = "horizontal",
-    layout_defaults = {
+    layout_config = {
       horizontal = {
         mirror = false,
       },
       vertical = {
         mirror = false
-      }
+      },
+      height = 1,
+      width = 0.75,
+      prompt_position = "bottom",
     },
+    border = {},
     file_sorter = require "telescope.sorters".get_fuzzy_file,
     file_ignore_patterns = {},
     generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
     shorten_path = true,
     winblend = 0,
-    width = 0.75,
-    results_height = 1,
-    results_width = 0.8,
-    border = {},
     borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
     color_devicons = true,
     use_less = true,
