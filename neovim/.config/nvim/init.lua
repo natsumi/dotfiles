24ea3097cbe80df21ccf85b52190088ae582a2f3
natsumi <natsumi@gmail.com>
2021-07-04 22:42:25 -0700

    format

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -6,18 +6,18 @@ require "settings"
 local fn = vim.fn
 
 -- Auto install packer.nvim
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.isdirectory(install_path) == 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  require('plugin_specification')
+  fn.system({"git", "clone", "https://github.com/wbthomason/packer.nvim", install_path})
+  require("plugin_specification")
   vim.cmd 'autocmd User PackerComplete ++once lua require("plugins")'
-  require('packer').sync()
+  require("packer").sync()
 else
-  require('plugins')
+  require("plugins")
 end
 
 require "themes"
 
 -- Plugin Configuration
-require 'file-icons' -- Icons for nvimTree
+require "file-icons" -- Icons for nvimTree
