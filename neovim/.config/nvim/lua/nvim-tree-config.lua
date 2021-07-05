0d72138c06361c0b71379092526a9cb7b0a23df3
natsumi <natsumi@gmail.com>
2021-07-04 20:26:53 -0700

    allow dotfiles

--- neovim/.config/nvim/lua/nvim-tree-config.lua
+++ neovim/.config/nvim/lua/nvim-tree-config.lua
@@ -3,14 +3,14 @@ local g = vim.g
 vim.o.termguicolors = true
 
 g.nvim_tree_side = "left"
-g.nvim_tree_width = 26
+g.nvim_tree_width = 40
 g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
 g.nvim_tree_auto_open = 0
 g.nvim_tree_auto_close = 0
 g.nvim_tree_quit_on_open = 0
 g.nvim_tree_follow = 1
 g.nvim_tree_indent_markers = 1
-g.nvim_tree_hide_dotfiles = 1
+g.nvim_tree_hide_dotfiles = 0
 g.nvim_tree_git_hl = 1
 g.nvim_tree_root_folder_modifier = ":t"
 g.nvim_tree_tab_open = 0
