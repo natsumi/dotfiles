f4e2feb6e38f92af8698b2b08a754d8af4676109
natsumi <natsumi@gmail.com>
2021-11-01 10:19:03 -0700

    update config

--- neovim/.config/nvim/lua/nvim-tree-config.lua
+++ neovim/.config/nvim/lua/nvim-tree-config.lua
@@ -13,7 +13,6 @@ g.nvim_tree_git_hl = 1
 g.nvim_tree_gitignore = 0
 g.nvim_tree_highlight_opened_files = 0
 g.nvim_tree_indent_markers = 1
-g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
 g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
 g.nvim_tree_root_folder_modifier = table.concat {":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??"}
 --
@@ -60,6 +59,7 @@ nvimtree.setup {
     }
   },
   filters = {
+    custom = {".git", "node_modules", ".cache"},
     dotfiles = false
   },
   disable_netrw = true,
