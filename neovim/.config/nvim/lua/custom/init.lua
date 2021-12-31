0d6897b6340f1d0ceb5926638e48aa8f54142172
natsumi <natsumi@gmail.com>
2021-12-31 01:08:26 -0800

    dap configu

--- neovim/.config/nvim/lua/custom/init.lua
+++ neovim/.config/nvim/lua/custom/init.lua
@@ -17,6 +17,33 @@ hooks.add("setup_mappings", function(map)
         opt
     )
     map("n", "<leader>q", ":q <CR>", opt)
+
+    -- DAP
+    map("n", "<leader>dct", '<cmd>lua require"dap".continue()<CR>')
+    map("n", "<leader>dsv", '<cmd>lua require"dap".step_over()<CR>')
+    map("n", "<leader>dsi", '<cmd>lua require"dap".step_into()<CR>')
+    map("n", "<leader>dso", '<cmd>lua require"dap".step_out()<CR>')
+    map("n", "<leader>dtb", '<cmd>lua require"dap".toggle_breakpoint()<CR>')
+
+    map("n", "<leader>dsc", '<cmd>lua require"dap.ui.variables".scopes()<CR>')
+    map("n", "<leader>dhh", '<cmd>lua require"dap.ui.variables".hover()<CR>')
+    map("v", "<leader>dhv", '<cmd>lua require"dap.ui.variables".visual_hover()<CR>')
+
+    map("n", "<leader>duh", '<cmd>lua require"dap.ui.widgets".hover()<CR>')
+    map("n", "<leader>duf", "<cmd>lua local widgets=require'dap.ui.widgets';widgets.centered_float(widgets.scopes)<CR>")
+
+    map("n", "<leader>dsbr", '<cmd>lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>')
+    map("n", "<leader>dsbm", '<cmd>lua require"dap".set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<CR>')
+    map("n", "<leader>dro", '<cmd>lua require"dap".repl.open()<CR>')
+    map("n", "<leader>drl", '<cmd>lua require"dap".repl.run_last()<CR>')
+    -- DAP Telescope
+    map("n", "<leader>dcc", '<cmd>lua require"telescope".extensions.dap.commands{}<CR>')
+    map("n", "<leader>dco", '<cmd>lua require"telescope".extensions.dap.configurations{}<CR>')
+    map("n", "<leader>dlb", '<cmd>lua require"telescope".extensions.dap.list_breakpoints{}<CR>')
+    map("n", "<leader>dv", '<cmd>lua require"telescope".extensions.dap.variables{}<CR>')
+    map("n", "<leader>df", '<cmd>lua require"telescope".extensions.dap.frames{}<CR>')
+    -- DAP UI
+    map("n", "<leader>dui", '<cmd>lua require"dapui".toggle()<CR>')
 end)
 
 -- NOTE : opt is a variable  there (most likely a table if you want multiple options),
@@ -96,6 +123,47 @@ hooks.add("install_plugins", function(use)
     })
     use({
         "tpope/vim-surround",
+    })
+
+    --
+    -- Debuggers
+    --
+    use({
+        "mfussenegger/nvim-dap",
+        requires = {
+            {
+                "suketa/nvim-dap-ruby",
+                after = "nvim-dap",
+                config = function()
+                    require("dap-ruby").setup()
+                end,
+            },
+            {
+                "rcarriga/nvim-dap-ui", -- Nice UI for debugging
+                after = "nvim-dap",
+                config = function()
+                    local dap, dapui = require("dap"), require("dapui")
+                    dap.listeners.after.event_initialized["dapui_config"] = function()
+                        dapui.open()
+                    end
+                    dap.listeners.before.event_terminated["dapui_config"] = function()
+                        dapui.close()
+                    end
+                    dap.listeners.before.event_exited["dapui_config"] = function()
+                        dapui.close()
+                    end
+                    require("dapui").setup()
+                end,
+            },
+        },
+    })
+
+    use({
+        "nvim-telescope/telescope-dap.nvim", -- Show project dependant history
+        after = "telescope.nvim",
+        config = function()
+            require("telescope").load_extension("dap")
+        end,
     })
 end)
 
