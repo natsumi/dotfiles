42a872b1bd26c28997e60a3ba041f2fba64b3843
natsumi <natsumi@gmail.com>
2022-01-03 11:58:05 -0800

    float diagnostic windows

--- neovim/.config/nvim/lua/custom/plugins/lspconfig.lua
+++ neovim/.config/nvim/lua/custom/plugins/lspconfig.lua
@@ -1,3 +1,16 @@
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = false,
+})
+
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+vim.o.updatetime = 250
+vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, border="single"})]])
+
 local M = {}
 
 M.setup_lsp = function(attach, capabilities)
