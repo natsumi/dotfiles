1530a27c68803cee0d4680fe756b437f4f386532
natsumi <natsumi@gmail.com>
2022-05-14 21:52:29 -0700

    update mapping

--- neovim/.config/nvim/lua/custom/mappings.lua
+++ neovim/.config/nvim/lua/custom/mappings.lua
@@ -1,41 +1,63 @@
 -- MAPPINGS
-local map = require("core.utils").map
+local M = {}
 
-map(
-    "n",
-    "<C-p>",
-    -- [[<cmd>lua require('telescope.builtin').find_files({find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<CR>]],
-    -- [[<cmd>Telescope find_files<cr>]],
-    [[<cmd>Telescope find_files find_command=rg,--hidden,--files<CR>]],
-    opt
-)
-map("n", "<leader>q", ":q <CR>", opt)
+-- add this table only when you want to disable default keys
+-- M.disabled = {
+--   n = {
+--       ["<leader>h"] = "",
+--       ["<C-s>"] = ""
+--   }
+-- },
+
+-- Mapping Format
+--  ["keys"] = {"action", "icon  mapping description"}
+
+-- map(
+--     "n",
+--     "<C-p>",
+--     -- [[<cmd>lua require('telescope.builtin').find_files({find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<CR>]] =
+--     -- [[<cmd>Telescope find_files<cr>]] =
+--     [[<cmd>Telescope find_files find_command=rg,--hidden,--files<CR>]] =
+--     opt
+-- )
+-- '["<leader>q", ":q <CR>", opt)
 
 -- DAP
-map("n", "<leader>dct", '<cmd>lua require"dap".continue()<CR>')
-map("n", "<leader>dsv", '<cmd>lua require"dap".step_over()<CR>')
-map("n", "<leader>dsi", '<cmd>lua require"dap".step_into()<CR>')
-map("n", "<leader>dso", '<cmd>lua require"dap".step_out()<CR>')
-map("n", "<leader>dtb", '<cmd>lua require"dap".toggle_breakpoint()<CR>')
+M.dap =  {
+   n = {
+ [ "<leader>dct" ] =  { '<cmd>lua require"dap".continue()<CR>', "DAP Continue" },
+ [ "<leader>dsv" ] = { '<cmd>lua require"dap".step_over()<CR>', "DAP Step Over" },
+ [ "<leader>dsi" ] =  { '<cmd>lua require"dap".step_into()<CR>', "DAP Step Into" },
+ [ "<leader>dso" ] =  { '<cmd>lua require"dap".step_out()<CR>', "DAP Step Out" },
+ [ "<leader>dtb" ] =  { '<cmd>lua require"dap".toggle_breakpoint()<CR>', "DAP Toogle Breakpoint" },
 
-map("n", "<leader>dsc", '<cmd>lua require"dap.ui.variables".scopes()<CR>')
-map("n", "<leader>dhh", '<cmd>lua require"dap.ui.variables".hover()<CR>')
-map("v", "<leader>dhv", '<cmd>lua require"dap.ui.variables".visual_hover()<CR>')
+ [ "<leader>dsc" ] =  { '<cmd>lua require"dap.ui.variables".scopes()<CR>', "DAP Variable Scope" },
+ [ "<leader>dhh" ] =  { '<cmd>lua require"dap.ui.variables".hover()<CR>', "DAP Variable Hover" },
 
-map("n", "<leader>duh", '<cmd>lua require"dap.ui.widgets".hover()<CR>')
-map("n", "<leader>duf", "<cmd>lua local widgets=require'dap.ui.widgets';widgets.centered_float(widgets.scopes)<CR>")
+ [ "<leader>duh" ] =  { '<cmd>lua require"dap.ui.widgets".hover()<CR>', "DAP Wiedget Hover" },
+ [ "<leader>duf" ] =  { "<cmd>lua local widgets=require'dap.ui.widgets';widgets.centered_float(widgets.scopes)<CR>",  "DAP Widgets" },
 
-map("n", "<leader>dsbr", '<cmd>lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>')
-map("n", "<leader>dsbm", '<cmd>lua require"dap".set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<CR>')
-map("n", "<leader>dro", '<cmd>lua require"dap".repl.open()<CR>')
-map("n", "<leader>drl", '<cmd>lua require"dap".repl.run_last()<CR>')
+ [ "<leader>dsbr" ] =  { '<cmd>lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>', "DAP Set Breakpoint" },
+ [ "<leader>dsbm" ] =  { '<cmd>lua require"dap".set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<CR>', "DAP Breakpoint Message " },
+ [ "<leader>dro" ] =  { '<cmd>lua require"dap".repl.open()<CR>', "DAP Repl Open" },
+ [ "<leader>drl" ] =  { '<cmd>lua require"dap".repl.run_last()<CR>', "DAP Repl Run Last" },
+   },
+
+   v = {
+ [ "<leader>dhv" ] =  { '<cmd>lua require"dap.ui.variables".visual_hover()<CR>', "DAP Variable Visual Hover" },
+   },
+}
+
 -- DAP Telescope
-map("n", "<leader>dcc", '<cmd>lua require"telescope".extensions.dap.commands{}<CR>')
-map("n", "<leader>dco", '<cmd>lua require"telescope".extensions.dap.configurations{}<CR>')
-map("n", "<leader>dlb", '<cmd>lua require"telescope".extensions.dap.list_breakpoints{}<CR>')
-map("n", "<leader>dv", '<cmd>lua require"telescope".extensions.dap.variables{}<CR>')
-map("n", "<leader>df", '<cmd>lua require"telescope".extensions.dap.frames{}<CR>')
--- DAP UI
-map("n", "<leader>dui", '<cmd>lua require"dapui".toggle()<CR>')
+M.telescope =  {
 
--- NOTE: the 4th argument in the map function can be a table i.e options but its most likely un-needed so dont worry about it
+["<leader>dcc"] = {'<cmd>lua require"telescope".extensions.dap.commands{}<CR>'},
+["<leader>dco"] = {'<cmd>lua require"telescope".extensions.dap.configurations{}<CR>'},
+["<leader>dlb"] = {'<cmd>lua require"telescope".extensions.dap.list_breakpoints{}<CR>'},
+["<leader>dv"] = {'<cmd>lua require"telescope".extensions.dap.variables{}<CR>'},
+["<leader>df"] = {'<cmd>lua require"telescope".extensions.dap.frames{}<CR>'},
+-- DAP UI
+["<leader>dui"] = {'<cmd>lua require"dapui".toggle()<CR>'},
+}
+
+return M
