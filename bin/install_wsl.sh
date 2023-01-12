41197dd8b82dc53e67bd544333b81f879f597ede
natsumi <natsumi@gmail.com>
2023-01-11 18:49:18 -0800

    wsl script no longer needed

--- bin/install_wsl.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-sudo apt-get update
-sudo apt-get upgrade
-
-sudo apt-get install build-essential
-sudo apt-get install git
-sudo apt-get install zsh
-sudo apt-get install vim neovim
