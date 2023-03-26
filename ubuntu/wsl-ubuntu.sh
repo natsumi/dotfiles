c4183be4432c9898f76a274f619c0f72231ea276
natsumi <natsumi@gmail.com>
2023-03-26 11:00:46 -0700

    add libpq

--- ubuntu/wsl-ubuntu.sh
+++ ubuntu/wsl-ubuntu.sh
@@ -11,28 +11,28 @@ sudo apt upgrade
 
 # Build tools
 sudo apt install -y \
-  automake autoconf libreadline-dev \
-  libncurses-dev libssl-dev libyaml-dev \
-  libxslt-dev libffi-dev libtool unixodbc-dev \
-  build-essential openssl libssl-dev
+	automake autoconf libreadline-dev \
+	libncurses-dev libssl-dev libyaml-dev \
+	libxslt-dev libffi-dev libtool unixodbc-dev \
+	build-essential openssl libssl-dev libpq-dev
 
 # Build Tools
 sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-  libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-  xz-utils tk-dev libffi-dev liblzma-dev git
+	libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+	xz-utils tk-dev libffi-dev liblzma-dev git
 
 # Utils
 sudo apt install -y \
-  htop jq fd-find fzf stow \
-  tig tmux ripgrep tree \
-  wget unzip curl vim neovim \
-  zsh wslu ffmpeg
+	htop jq fd-find fzf stow \
+	tig tmux ripgrep tree \
+	wget unzip curl vim neovim \
+	zsh wslu ffmpeg
 
 echo 'Installing asdf'
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.0
 
 echo 'Installing bat'
-curl -sL https://api.github.com/repos/sharkdp/bat/releases/latest  | jq -r '.assets[].browser_download_url' | grep amd | tail -n 1 | xargs wget
+curl -sL https://api.github.com/repos/sharkdp/bat/releases/latest | jq -r '.assets[].browser_download_url' | grep amd | tail -n 1 | xargs wget
 ls *.deb | sudo xargs dpkg -i
 
 echo 'Installing diff-so-fancy'
@@ -41,7 +41,7 @@ chmod +x diff-so-fancy
 sudo mv diff-so-fancy /usr/local/bin
 
 echo 'Installing scmpuff'
-curl -sL https://api.github.com/repos/mroth/scmpuff/releases/latest  | jq -r '.assets[].browser_download_url' | grep linux_x64 | xargs -I % wget % -O - |  tar xvz scmpuff
+curl -sL https://api.github.com/repos/mroth/scmpuff/releases/latest | jq -r '.assets[].browser_download_url' | grep linux_x64 | xargs -I % wget % -O - | tar xvz scmpuff
 chmod +x scmpuff
 sudo mv scmpuff /usr/local/bin
 
@@ -58,7 +58,7 @@ bash $DOTFILES_DIR/bin/apply_git_settings
 
 echo 'Installing Prezto'
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-git clone --recursive https://github.com/belak/prezto-contrib  "${ZDOTDIR:-$HOME}/.zprezto/contrib"
+git clone --recursive https://github.com/belak/prezto-contrib "${ZDOTDIR:-$HOME}/.zprezto/contrib"
 
 echo 'Installing Zplug'
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
